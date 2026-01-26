be416f79bca670adea08bbfeb8fab84230064721
Your Name <you@example.com>
2026-01-25 20:12:14 -0500

    deps: Update build.rs

--- x11/build.rs
+++ x11/build.rs
@@ -1,6 +1,5 @@
 // x11-rs: Rust bindings for X11 libraries
-// The X11 libraries are available under the MIT license.
-// These bindings are public domain.
+// Run cargo build with -vv to see output.
 
 #![allow(clippy::vec_init_then_push)]
 
