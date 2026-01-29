783128e466c6196c1084084fe203b22f18215ee9
Your Name <you@example.com>
2026-01-28 21:22:14 -0500

    deps: Document dependency diffs

---
+++ generate-diffs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+packages=(
+  "egui_software_backend"
+  "raw-window-handle"
+  "smithay-clipboard"
+  "softbuffer"
+  "winit"
+  "x11"
+)
+
+find diff-tmp -exec chmod u+w {} \;
+rm -r diff-tmp
+mkdir diff-tmp
+cd diff-tmp || exit 1
+
+[[ -f ../vendor.diff ]] && rm ../vendor.diff
+for pkg in "${packages[@]}"; do
+    cp -r ../upstream-for-reference/"$pkg" ./original-"$pkg"
+    cp -r ../"$pkg" ./"$pkg"
+    find . -exec chmod u+w {} \;
+    if [[ "$pkg" == "x11" ]] ; then
+        rm -r ./x11/c_src
+    elif [[ "$pkg" == "egui_software_backend" ]] ; then
+        rm -r ./original-egui_software_backend/examples
+        rm -r ./egui_software_backend/examples
+    fi
+    diff -uraN original-"$pkg" ./"$pkg"  >> ../vendor.diff || true
+done
+cd .. || exit 1
+rm -r diff-tmp
